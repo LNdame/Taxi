0018f68f1d348f4c1b725c478661067b4b37ce37
LNdame <ls20045@gmail.com>
2016-05-27 12:41:48 +0200

    added sample

---
+++ app/src/main/java/ansteph/com/taxi/model/sample.java
@@ -0,0 +1,26 @@
+package ansteph.com.taxi.model;
+
+/**
+ * Created by loicStephan on 27/05/16.
+ */
+public class sample {
+
+    private String name ;
+    private int number;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
